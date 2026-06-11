f265f3efce45ca1cab4759154db697383e5942f3
firstuser09 <workcopilot1@gmail.com>
2026-06-12 01:40:44 +0530

    desktop fixes

--- src/components/insights/LossChart/LossChart.tsx
+++ src/components/insights/LossChart/LossChart.tsx
@@ -73,7 +73,7 @@ export default function LossChart() {
       </div>
 
       <div className={styles.chartContainer}>
-        <ResponsiveContainer width="100%" height={200}>
+        <ResponsiveContainer width="100%" height="100%">
           <BarChart
             data={chartData}
             margin={{ top: 10, right: 10, left: -20, bottom: 0 }}
